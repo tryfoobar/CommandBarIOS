8d83aba59b5834aa475423300ec4fcc0f8489ba1
Michael Cavallaro <michaelcavallaro03@gmail.com>
2023-10-30 08:33:06 -0600

    Expose a delegate for receiving an action

--- Sources/CommandBarIOS/HelpHubWebView.swift
+++ Sources/CommandBarIOS/HelpHubWebView.swift
@@ -2,11 +2,13 @@ import UIKit
 import WebKit
 
 public class HelpHubWebView: WKWebView, WKNavigationDelegate, WKScriptMessageHandler {
-    // TODO: Make these configurable
     private var orgId: String = "foocorp"
     private var launchCode: String = "prod"
-    private var userId: String = "1234"
+    // TODO: Make these configurable
+    private var userId: String = "null"
     private var debug: Bool = true
+
+    public weak var delegate: HelpHubWebViewDelegate?
 
     public init(orgId: String, frame: CGRect) {
         self.orgId = orgId
@@ -20,7 +22,7 @@ public class HelpHubWebView: WKWebView, WKNavigationDelegate, WKScriptMessageHan
 
     func loadContent() {
         let userContentController = WKUserContentController()
-        userContentController.add(self, name: "onOpenSupportChat")
+        userContentController.add(self, name: "commandbar__onFallbackAction")
 
         configuration.userContentController = userContentController
         configuration.websiteDataStore = WKWebsiteDataStore.default()
@@ -51,10 +53,12 @@ public class HelpHubWebView: WKWebView, WKNavigationDelegate, WKScriptMessageHan
     }
 
     private func loadSnippet() {
+
         let snippet = """
             (function() {
                     var o="\(self.orgId)",n=["Object.assign","Symbol","Symbol.for"].join("%2C"),a=window;function t(o,n){void 0===n&&(n=!1),"complete"!==document.readyState&&window.addEventListener("load",t.bind(null,o,n),{capture:!1,once:!0});var a=document.createElement("script");a.type="text/javascript",a.async=n,a.src=o,document.head.appendChild(a)}function r(){var n;if(void 0===a.CommandBar){delete a.__CommandBarBootstrap__;var r=Symbol.for("CommandBar::configuration"),e=Symbol.for("CommandBar::orgConfig"),c=Symbol.for("CommandBar::disposed"),i=Symbol.for("CommandBar::isProxy"),m=Symbol.for("CommandBar::queue"),l=Symbol.for("CommandBar::unwrap"),d=[],s="\(self.launchCode)",u=s&&s.includes("local")?"http://localhost:8000":"https://api.commandbar.com",f=Object.assign(((n={})[r]={uuid:o},n[e]={},n[c]=!1,n[i]=!0,n[m]=new Array,n[l]=function(){return f},n),a.CommandBar),p=["addCommand","boot"],y=f;Object.assign(f,{shareCallbacks:function(){return{}},shareContext:function(){return{}}}),a.CommandBar=new Proxy(f,{get:function(o,n){return n in y?f[n]:p.includes(n)?function(){var o=Array.prototype.slice.call(arguments);return new Promise((function(a,t){o.unshift(n,a,t),f[m].push(o)}))}:function(){var o=Array.prototype.slice.call(arguments);o.unshift(n),f[m].push(o)}}}),null!==s&&d.push("lc=".concat(s)),d.push("version=2"),t("".concat(u,"/latest/").concat(o,"?").concat(d.join("&")),!0)}}void 0===Object.assign||"undefined"==typeof Symbol||void 0===Symbol.for?(a.__CommandBarBootstrap__=r,t("https://polyfill.io/v3/polyfill.min.js?version=3.101.0&callback=__CommandBarBootstrap__&features="+n)):r();
-                    window.CommandBar.boot("\(self.userId)", { products: ["help_hub"] });
+                    window.CommandBar.boot(\(self.userId), { products: ["help_hub"] });
+                    window._cbIsWebView = true;
                     window.CommandBar.openHelpHub();
             })();
         """
@@ -82,7 +86,10 @@ public class HelpHubWebView: WKWebView, WKNavigationDelegate, WKScriptMessageHan
             return
         }
 
-        // TODO: Bubble this event up
-        print(dict)
+        delegate?.didReceiveEvent(dict)
     }
+}
+
+public protocol HelpHubWebViewDelegate: AnyObject {
+    func didReceiveEvent(_ event: [String: Any])
 }
